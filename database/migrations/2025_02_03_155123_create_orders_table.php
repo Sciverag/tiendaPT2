40929c68338ed579ad6b72901d4a88b7b632ca57
Sciverag <samuelciga2004@gmail.com>
2025-02-03 17:16:25 +0100

    Creadas migraciones y controladores

--- database/migrations/2025_02_03_155123_create_orders_table.php
+++ database/migrations/2025_02_03_155123_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->string('nombre');
-            $table->string('apellidos');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('api_token', 60)->unique()->nullable();
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->date("fecha");
+            $table->string("dniCliente");
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 }
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
 
 <footer class="container-flow d-flex justify-content-end align-items-end bg-dark order-last">
-    <h1 class="fs-1">GameShop</h1>
+    <h1 class="text-white">&#169;​GameShop</h1>
 </footer>
